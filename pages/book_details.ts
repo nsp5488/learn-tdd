26fa9fdda927f3fe713ea7bbc31e764d70223aba
nsp5488 <nsp5488@g.rit.edu>
2024-11-07 16:21:49 -0500

    implemented all tests

--- pages/book_details.ts
+++ pages/book_details.ts
@@ -1,38 +1,36 @@
-import { Response } from 'express';
-import Book, { IBook }  from '../models/book';
-import BookInstance, { IBookInstance }  from '../models/bookinstance';
-
+import { Response } from "express";
+import Book, { IBook } from "../models/book";
+import BookInstance, { IBookInstance } from "../models/bookinstance";
 
 // Function to get a book by ID
 async function getBook(id: string): Promise<IBook | null> {
-  if (typeof id !== 'string') {
+  if (typeof id !== "string") {
     return null;
   }
-  return Book.findOne({ _id: id }).populate('author').exec();
+  return Book.findOne({ _id: id }).populate("author").exec();
 }
 
 // Function to get book details
 async function getBookDtl(id: string): Promise<IBookInstance[] | null> {
-  if (typeof id !== 'string') {
+  if (typeof id !== "string") {
     return null;
   }
-  return BookInstance.find({ book: id }).select('imprint status').exec();
+  return BookInstance.find({ book: id }).select("imprint status").exec();
 }
 
 // Function to handle showing book details
-export const showBookDtls = async (res: Response, id: string): Promise<void> => {
+export const showBookDtls = async (
+  res: Response,
+  id: string
+): Promise<void> => {
   try {
-    const [book, copies] = await Promise.all([
-      getBook(id),
-      getBookDtl(id)
-    ]);
-    
+    const [book, copies] = await Promise.all([getBook(id), getBookDtl(id)]);
     if (!book) {
       res.status(404).send(`Book ${id} not found`);
       return;
     }
 
-    if(!copies) {
+    if (!copies) {
       res.status(404).send(`Book details not found for book ${id}`);
       return;
     }
@@ -40,7 +38,7 @@ export const showBookDtls = async (res: Response, id: string): Promise<void> => 
     res.send({
       title: book.title,
       author: book.author.name,
-      copies: copies
+      copies: copies,
     });
   } catch (err) {
     res.status(500).send(`Error fetching book ${id}`);
